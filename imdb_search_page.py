961efe2d466cbf023c53cfd2524d46160abdd4b6
SushmaBolloju <160645457+SushmaBolloju@users.noreply.github.com>
2024-07-07 10:32:09 +0530

    Update imdb_search_page.py

--- imdb_search_page.py
+++ imdb_search_page.py
@@ -1,5 +1,6 @@
 import os
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,7 +13,19 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.imdb.com/search/name/")
 driver.maximize_window()
 driver.implicitly_wait(60)
-search_box = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, 'suggestion-search')))
-search_box.send_keys("Aadhya Anand")
-search_button = WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.XPATH, '//button[@id="suggestion-search-button"]')))
-search_button.click()
+try:
+    search_box = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, 'suggestion-search')))
+    search_box.send_keys("Aadhya Anand")
+    search_button = WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.XPATH, '//button[@id="suggestion-search-button"]')))
+    search_button.click()
+except NoSuchElementException as e:
+    print("Error")
+
+except Exception as e:
+# Handle other exceptions (TimeoutException, WebDriverException, etc.)
+    print(f"An error occurred: {e}")
+
+finally:
+    driver.quit()
+
+
